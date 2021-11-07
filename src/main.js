351b318bd42d980247f43b6751e0c7494db1d7db
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-07 18:25:57 -0300

    chore: init brand repository

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+          /*
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+          */
+        },
+      });
+    },
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
